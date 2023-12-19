9a94ec625ce8274f517a995b1c68cd58f61b4253
llsrrll96 <llsrrll96@gmail.com>
2023-12-19 11:12:02 +0900

    feat: dto 생성

---
+++ src/main/java/com/inflearn/optimization/domain/order/repository/query/OrderQueryDto.java
@@ -0,0 +1,27 @@
+package com.inflearn.optimization.domain.order.repository.query;
+
+
+import com.inflearn.optimization.domain.member.entity.Address;
+import com.inflearn.optimization.domain.order.entity.OrderStatus;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class OrderQueryDto {
+    private Long orderId;
+    private String name;
+    private LocalDateTime orderDate;
+    private OrderStatus orderStatus;
+    private Address address;
+    private List<OrderItemQueryDto> orderItems;
+
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+    }
+}
